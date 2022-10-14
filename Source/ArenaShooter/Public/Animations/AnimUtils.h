84334172c2fbde295b9b41211d4aa03447479d8c
gurgenkarapetyan <gurgen.karapetyan.noor@gmail.com>
2022-10-14 13:47:27 +0400

    - added core type and helper class

---
+++ Source/ArenaShooter/Public/Animations/AnimUtils.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+class AnimUtils
+{
+public:
+	/** static helper function to find anim notify in the animation.
+	 * @param Animation to find the notify.
+	 */
+	template <typename T>
+	static T* FindNotifyByClass(UAnimSequenceBase* Animation)
+	{
+		if (Animation == nullptr)
+		{
+			return nullptr;
+		}
+	
+		const TArray<FAnimNotifyEvent> NotifyEvents = Animation->Notifies;
+		for (const FAnimNotifyEvent NotifyEvent : NotifyEvents)
+		{
+			T* AnimNotify = Cast<T>(NotifyEvent.Notify);
+			if (AnimNotify)
+			{
+				return AnimNotify;
+			}
+		}
+		
+		return nullptr;
+	}
+};
---
+++ Source/ArenaShooter/Public/CoreTypes.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreTypes.generated.h"
+
+class ABaseWeapon;
+
+USTRUCT(BlueprintType)
+struct FAmmoData
+{
+	GENERATED_USTRUCT_BODY()
+
+	/** Number of bullets that weapons has in the clip. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	int32 Bullets;
+
+	/** Number of clips. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components", meta = (EditCondition = "!Infinite"))
+	int32 Clips;
+
+	/** If weapon has an infinite number of clips. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	bool Infinite;
+};
+
+USTRUCT(BlueprintType)
+struct FWeaponData
+{
+	GENERATED_USTRUCT_BODY()
+
+	/**  Weapon class blueprint. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	TSubclassOf<ABaseWeapon> WeaponClass;
+	
+	/**  Based on weapon type reload anim montage. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
+	UAnimMontage* ReloadAnimMontage;
+};
